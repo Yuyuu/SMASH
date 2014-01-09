f38176a07b8d7cc3183d7715dcd0f6aa82293398
vter <color.ui>
2014-01-09 13:49:00 +0100

    Fixed geb installation and BuildConfig for tests execution (grails.project.fork)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,19 +7,19 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-//grails.project.fork = [
-//    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-//    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-//
-//    // configure settings for the test-app JVM, uses the daemon by default
-//    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-//    // configure settings for the run-app JVM
-//    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-//    // configure settings for the run-war JVM
-//    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-//    // configure settings for the Console UI JVM
-//    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-//]
+/*grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]*/
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
@@ -52,6 +52,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.24'
+        test "org.gebish:geb-spock:0.9.2"
     }
 
     plugins {
@@ -62,7 +63,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
         compile ':spring-security-core:2.0-RC2'
-		//compile ":geb:0.9.2"
+		compile ":geb:0.9.2"
         compile ":codenarc:0.19"
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
@@ -75,5 +76,6 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
         test ":code-coverage:1.2.7"
+        test("org.grails.plugins:geb:0.9.2")
     }
 }
--- test/unit/com/smash/domain/CommentSpec.groovy
+++ test/unit/com/smash/domain/CommentSpec.groovy
@@ -2,6 +2,7 @@ package com.smash.domain
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,15 @@ import spock.lang.Specification
 @TestFor(Comment)
 class CommentSpec extends Specification {
 
-    def setup() {
-    }
+    @Unroll
+    void "dummyTest"() {
+        expect:
+        res == a + b
 
-    def cleanup() {
-    }
-
-    void "test something"() {
+        where:
+        a  | b  | res
+        1  | 1  | 2
+        2  | 3  | 5
+        23 | 10 | 33
     }
 }
