dafcf9ba62d872653e586c4180b6f87bfa581b6c
KevinS <lepiaf31@gmail.com>
2014-01-09 13:36:12 +0100

    Remove build config fork property

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,19 +7,19 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
+//grails.project.fork = [
+//    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+//    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+//
+//    // configure settings for the test-app JVM, uses the daemon by default
+//    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+//    // configure settings for the run-app JVM
+//    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//    // configure settings for the run-war JVM
+//    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//    // configure settings for the Console UI JVM
+//    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+//]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
@@ -62,7 +62,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
         compile ':spring-security-core:2.0-RC2'
-		compile ":geb:0.9.2"
+		//compile ":geb:0.9.2"
         compile ":codenarc:0.19"
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
