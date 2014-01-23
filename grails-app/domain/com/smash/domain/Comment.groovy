699dd4caff1f45d5352403ecde9faac224519ac4
sandreea <soare.andreea.georgiana@gmail.com>
2014-01-23 10:29:45 +0100

    plugins updates

--- grails-app/domain/com/smash/domain/Comment.groovy
+++ grails-app/domain/com/smash/domain/Comment.groovy
@@ -8,7 +8,13 @@ class Comment {
 	static belongsTo = [media: MediaCut];
 
     static constraints = {
-		text blank: false 
+		text blank: false, size: 1..250
 		author nullable: false
+        media nullable: false
     }
+
+    String getComment(){
+        text
+    }
+
 }
