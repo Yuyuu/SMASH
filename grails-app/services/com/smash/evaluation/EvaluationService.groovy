b9dedf19fa9fb8100c19db182b42c7de64ef0ada
Axel Robert <robert.axel31@gmail.com>
2014-02-09 13:02:27 +0100

    Evaluation service class implementation #8

---
+++ grails-app/services/com/smash/evaluation/EvaluationService.groovy
@@ -0,0 +1,18 @@
+package com.smash.evaluation
+
+import com.smash.domain.Evaluation
+import com.smash.media.MediaCut
+import grails.transaction.Transactional
+
+@Transactional
+class EvaluationService {
+
+    def voteForMediaCut(MediaCut mediacut, Evaluation evaluation) {
+        if(evaluation.getVote())
+            mediacut.setNbPositiveVote(++mediacut.getNbPositiveVote())
+        else
+            mediacut.setNbNegativeVote(--mediacut.getNbNegativeVote())
+        mediacut.addToEvaluations(evaluation)
+        mediacut.save(flush: true)
+    }
+}
